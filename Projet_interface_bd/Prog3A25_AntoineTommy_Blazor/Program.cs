3a1e2276db363ed1f577fff588cd0f4a52453c88
Antoine Simard <2433883@etu.cegepjonquiere.ca>
2025-11-04 09:15:05 -0500

    connexion fonctionnelle

--- Projet_interface_bd/Prog3A25_AntoineTommy_Blazor/Program.cs
+++ Projet_interface_bd/Prog3A25_AntoineTommy_Blazor/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Prog3A25_AntoineTommy_Blazor.Components;
 using Prog3A25_AntoineTommy_Blazor.Data;
+using Prog3A25_AntoineTommy_Blazor.Services;
 
 // ajouter comme paramètre aux constructeurs de services :
 // public UserAccountService(IDbContextFactory<Prog3A25AntoineTommyProdContext> factory)
@@ -15,9 +16,10 @@ namespace Prog3A25_AntoineTommy_Blazor
             var builder = WebApplication.CreateBuilder(args);
             var conStrBuilder =
                 new SqlConnectionStringBuilder(builder.Configuration.GetConnectionString("Connexion"));
-            conStrBuilder.Password = builder.Configuration["NomProf"];
+            conStrBuilder.Password = builder.Configuration["MotPasse"];
             builder.Services.AddPooledDbContextFactory<Prog3A25AntoineTommyProdContext>(
                 x => x.UseSqlServer(conStrBuilder.ConnectionString));
+            builder.Services.AddScoped<LoginService>();
             // Add services to the container.
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
