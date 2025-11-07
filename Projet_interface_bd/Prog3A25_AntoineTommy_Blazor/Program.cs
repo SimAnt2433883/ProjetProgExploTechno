7c69c1c2c5a754084798419a90112f810e9c8252
Antoine Simard <2433883@etu.cegepjonquiere.ca>
2025-11-07 14:07:13 -0500

    connexion fonction finale sans token

--- Projet_interface_bd/Prog3A25_AntoineTommy_Blazor/Program.cs
+++ Projet_interface_bd/Prog3A25_AntoineTommy_Blazor/Program.cs
@@ -15,8 +15,10 @@ namespace Prog3A25_AntoineTommy_Blazor
         {
             var builder = WebApplication.CreateBuilder(args);
             var conStrBuilder =
-                new SqlConnectionStringBuilder(builder.Configuration.GetConnectionString("Connexion"));
-            conStrBuilder.Password = builder.Configuration["MotPasse"];
+                new SqlConnectionStringBuilder(builder.Configuration.GetConnectionString("Connexion"))
+                {
+                    Password = builder.Configuration["MotPasse"]
+                };
             builder.Services.AddPooledDbContextFactory<Prog3A25AntoineTommyProdContext>(
                 x => x.UseSqlServer(conStrBuilder.ConnectionString));
             builder.Services.AddScoped<LoginService>();
--- Projet_interface_bd/Prog3A25_AntoineTommy_Blazor/Services/LoginService.cs
+++ Projet_interface_bd/Prog3A25_AntoineTommy_Blazor/Services/LoginService.cs
@@ -1,31 +1,36 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Query;
 using Prog3A25_AntoineTommy_Blazor.Data;
 using Prog3A25_AntoineTommy_Blazor.Models;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
 
 namespace Prog3A25_AntoineTommy_Blazor.Services
 {
-    public class LoginService
+    public class LoginService(IDbContextFactory<Prog3A25AntoineTommyProdContext> factory)
     {
-        private readonly IDbContextFactory<Prog3A25AntoineTommyProdContext> factory;
+        private readonly IDbContextFactory<Prog3A25AntoineTommyProdContext> factory = factory;
 
-        public LoginService(IDbContextFactory<Prog3A25AntoineTommyProdContext> factory)
-        {
-            this.factory = factory;
-        }
-
-        public async Task<Utilisateur?> VerifierConnexion(string email, string motPasse)
+        public async Task<string?> ConnecterUtilisateur(string email, string motPasse)
         {
             var db = await factory.CreateDbContextAsync();
 
+            int? noUtilisateur = await ExecuterConnexion(db, email, motPasse);
+
+            Utilisateur? utilisateur = GetUtilisateur(db, noUtilisateur);
+            return GetBaliseUtilisateur(utilisateur);
+        }
+
+        private static async Task<int?> ExecuterConnexion(Prog3A25AntoineTommyProdContext db, string email, string motPasse)
+        {
             var emailParam = new SqlParameter("@email", email);
             var mdpParam = new SqlParameter("@mdp", motPasse);
-            var reponseParam = new SqlParameter("@reponse", System.Data.SqlDbType.Int)
+            var reponseParam = new SqlParameter("@reponse", SqlDbType.Int)
             {
-                Direction = System.Data.ParameterDirection.Output
+                Direction = ParameterDirection.Output
             };
 
             await db.Database.ExecuteSqlRawAsync(
@@ -33,24 +38,38 @@ namespace Prog3A25_AntoineTommy_Blazor.Services
                 emailParam, mdpParam, reponseParam
             );
 
-            int noUtilisateur = reponseParam.Value == DBNull.Value ? -1 : (int)reponseParam.Value;
+            return reponseParam.Value == DBNull.Value ? -1 : (int)reponseParam.Value;
+        }
 
-            Console.WriteLine(noUtilisateur);
 
-            if (noUtilisateur == -1)
-                return null;
-
-            List<Utilisateur> utilisateursLinq = 
+        private static Utilisateur? GetUtilisateur(Prog3A25AntoineTommyProdContext db, int? noUtilisateur)
+        {
+            List<Utilisateur> utilisateurs =
                 [.. from utilisateur in db.Utilisateurs
                     where utilisateur.NoUtilisateur == noUtilisateur
                     select utilisateur];
 
             Utilisateur? utilisateurRetour = null;
-
-            foreach (Utilisateur utilisateurConnecte in utilisateursLinq)
+            foreach (Utilisateur utilisateurConnecte in utilisateurs)
                 utilisateurRetour = utilisateurConnecte;
 
             return utilisateurRetour;
         }
+
+        private static string? GetBaliseUtilisateur(Utilisateur? utilisateur)
+        {
+            if (utilisateur == null)
+                return "<p class='text-danger fw-semibold mt-3'>Erreur de connexion de l'utilisateur.</p>";
+            else
+                return $@"<table class='table table-striped table-bordered mt-3'>" +
+                                $"<tbody>" +
+                                    $"<tr><th scope='row'>NoUtilisateur</th><td>{utilisateur.NoUtilisateur}</td></tr>" +
+                                    $"<tr><th scope='row'>Nom</th><td>{utilisateur.Nom}</td></tr>" +
+                                    $"<tr><th scope='row'>Email</th><td>{utilisateur.Email}</td></tr>" +
+                                    $"<tr><th scope='row'>Administrateur</th><td>{(utilisateur.Administrateur ? "Oui" : "Non")}</td></tr>" +
+                                $"</tbody>" +
+                            $"</table>";
+        }
+
     }
 }
