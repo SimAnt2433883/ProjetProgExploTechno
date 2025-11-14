67fc548a18892ed716f553630d91d47961f56d39
Tommy Girard <2433520@etu.cegepjonquiere.ca>
2025-11-14 13:35:56 -0500

    inscription commence

---
+++ Projet_interface_bd/Prog3A25_AntoineTommy_Blazor/Models/InscriptionModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Prog3A25_AntoineTommy_Blazor.Models
+{
+    public class InscriptionModel
+    {
+        public string Nom { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string MotPasse { get; set; } = "";
+    }
+}
+
---
+++ Projet_interface_bd/Prog3A25_AntoineTommy_Blazor/Services/InscriptionService.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Prog3A25_AntoineTommy_Blazor.Data;
+using Prog3A25_AntoineTommy_Blazor.Models;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace Prog3A25_AntoineTommy_Blazor.Services
+{
+    public class InscriptionService
+    {
+        private readonly IDbContextFactory<Prog3A25AntoineTommyProdContext> _factory;
+
+        public InscriptionService(IDbContextFactory<Prog3A25AntoineTommyProdContext> factory)
+        {
+            _factory = factory;
+        }
+
+        public async Task<Utilisateur> InscrireUtilisateur(string email, string nom, string motPasse)
+        {
+            try
+            {
+                using var db = await _factory.CreateDbContextAsync();
+
+                var emailParam = new SqlParameter("@email", email);
+                var nomParam = new SqlParameter("@nom", nom);
+                var mdpParam = new SqlParameter("@motPasse", motPasse);
+                var reponseParam = new SqlParameter("@reponse", System.Data.SqlDbType.Int)
+                {
+                    Direction = System.Data.ParameterDirection.Output
+                };
+
+                await db.Database.ExecuteSqlRawAsync(
+                    "EXEC Inscription @email, @nom, @motPasse, @reponse OUTPUT",
+                    emailParam, nomParam, mdpParam, reponseParam
+                );
+
+                int result = reponseParam.Value == DBNull.Value ? -1 : (int)reponseParam.Value;
+
+                if (result > 0)
+                {
+                    var utilisateur = await db.Utilisateurs.FirstOrDefaultAsync(u => u.NoUtilisateur == result);
+                    return utilisateur;
+                }
+                return null;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
